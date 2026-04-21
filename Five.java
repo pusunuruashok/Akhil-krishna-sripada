cf745119fe5c86862f7aa7f23ee44370ea75b239
Ashok Pusunuru <pusunuruashok99@gmail.com>
2026-04-20 20:16:16 -0400

    the hidden details in the system

---
+++ Five.java
@@ -0,0 +1,8 @@
+public class Five {
+    public static void main(String[] args) {
+     
+        System.out.println("Hello, World!");
+        System.out.println("Welcome to Java programming.");
+        System.out.println("Java is a versatile and powerful programming language.");
+    }
+}
